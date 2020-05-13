5a46a7d327fcf87183853da162a8291f355bdbf3
olucheye <seyefree2rhyme@yahoo.com>
2020-05-13 16:20:31 +0100

    prototypes and callbacs solved

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,14 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
 
+// Explanation: 
+`Nested function is able to access the variable internal becuase the both in the same local scope of myFunction(). If nestedFunction is moved outside of the myFunction scope, it would no longer be access the internal variable, save the variable is then moved out as a globally defined variable like the external variable.`
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+const sumation = (num) => {
+  
+}
