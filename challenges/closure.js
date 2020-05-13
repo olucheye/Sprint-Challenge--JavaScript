05f3b2b3381d74b68dec1b2bd4c1894cbf5b028b
olucheye <seyefree2rhyme@yahoo.com>
2020-05-13 17:00:28 +0100

    mvp challenges solved under time

--- challenges/closure.js
+++ challenges/closure.js
@@ -24,6 +24,12 @@ myFunction();
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
-const sumation = (num) => {
-  
+function sumation(num){
+  let total = 0;
+  for (let i = 0; i <= num; i++) {
+    total +=i;
+  }
+  return total;
 }
+
+console.log(sumation(4));
